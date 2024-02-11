b6eed482d6b2e77316887a0ca847515f74a6cdca
David Stevenson <drstevenson1958@yahoo.com>
2024-02-11 14:53:09 -0500

    Prepare the Product Data Web API service

---
+++ ch06/SportsStore/Controllers/ProductsController.cs
@@ -0,0 +1,22 @@
+﻿using SportsStore.Models;
+using System.Collections.Generic;
+using System.Web.Http;
+using System.Net;
+using System.Net.Http;
+using System;
+using Newtonsoft.Json;
+
+namespace SportsStore.Controllers
+{
+    public class ProductsController : ApiController
+    {
+        [Route("products")]
+        [HttpGet]
+        public IEnumerable<Product> GetProducts()
+        {
+            //throw new Exception("Failed to get data from repository.");
+            //throw new HttpResponseException(HttpStatusCode.BadRequest);
+            return Repository.Products;
+        }
+    }
+}
