2389085299951288d5165ee9dae80c8054f61d7e
David Stevenson <drstevenson1958@yahoo.com>
2024-02-11 15:57:04 -0500

    Listing 6-6. The contents of the productListControllers.js file

---
+++ ch06/SportsStore/Content/controllers/productListControllers.js
@@ -0,0 +1,14 @@
+﻿angular.module("sportsStore")
+	.controller("productListCtrl", function ($scope, $filter) {
+
+		let selectedCategory = null;
+
+		$scope.selectCategor = function (newCategory) {
+			selectedCategory = newCategory;
+		}
+
+		$scope.categoryFilterFn = function (product) {
+			return selectedCategory == null ||
+				product.category == selectedCategory;
+		}
+	});
