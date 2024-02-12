9cdc74e35e2453028b454398b745c21d89b674fb
David Stevenson <drstevenson1958@yahoo.com>
2024-02-12 17:30:55 -0500

    Listing 6-10. Updating the Controller to support pagination, as shown in Listing 6-10.

--- ch06/SportsStore/Content/controllers/productListControllers.js
+++ ch06/SportsStore/Content/controllers/productListControllers.js
@@ -1,11 +1,16 @@
 ﻿angular.module("sportsStore")
 	.constant("productListActiveClass", "btn-primary")
-	.controller("productListCtrl", function ($scope, $filter, productListActiveClass) {
+	.constant("productListPageCount", 3)
+	.controller("productListCtrl", function ($scope, $filter, productListActiveClass, productListPageCount) {
 
 		let selectedCategory = null;
 
+		$scope.selectedPage = 1;
+		$scope.pageSize = productListPageCount;
+
 		$scope.selectCategory = function (newCategory) {
 			selectedCategory = newCategory;
+			$scope.selectedPage = 1;
 		}
 
 		$scope.categoryFilterFn = function (product) {
@@ -15,5 +20,9 @@
 
 		$scope.getCategoryClass = function (category) {
 			return selectedCategory == category ? productListActiveClass : "";
+		}
+
+		$scope.getPageClass = function (page) {
+			return $scope.selectedPage == page ? productListActiveClass : "";
 		}
 	});
