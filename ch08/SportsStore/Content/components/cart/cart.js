7ba1861c346b7a8ff2dd9429a5d89fc48bc3fde4
David Stevenson <drstevenson1958@yahoo.com>
2024-02-17 14:26:33 -0500

    Chapter 8: SportsStore: Orders and Administration

---
+++ ch08/SportsStore/Content/components/cart/cart.js
@@ -0,0 +1,62 @@
+﻿angular.module("cart", [])
+	.factory("cart", function () {
+
+		let cartData = [];
+
+		return {
+			addProduct: function (id, name, price) {
+				let addedToExistingItem = false;
+				for (let item of cartData) {
+					if (item.id == id) {
+						item.count++;
+						addedToExistingItem = true;
+						break;
+					}
+				}
+
+				if (!addedToExistingItem) {
+					cartData.push({
+						count: 1, id: id, price: price, name: name
+					});
+				}
+			},
+
+			removeProduct: function (id) {
+				for (let i = 0; i < cartData.length; i++) {
+					if (cartData[i].id == id) {
+						cartData.splice(i, 1);
+					}
+				}
+			},
+
+			getProducts: function () {
+				return cartData;
+			}
+		}
+	})
+	.directive("cartSummary", function (cart) {
+		return {
+			restrict: "E",
+			templateUrl: "components/cart/cartSummary.html",
+			controller: function ($scope) {
+
+				let cartData = cart.getProducts();
+
+				$scope.total = function () {
+					let total = 0;
+					for (let item of cartData) {
+						total += item.price * item.count;
+					}
+					return total;
+				}
+
+				$scope.itemCount = function () {
+					let total = 0;
+					for (let item of cartData) {
+						total += item.count;
+					}
+					return total;
+				}
+			}
+		}
+	});
---
+++ ch08/SportsStore/Content/controllers/checkoutControllers.js
@@ -0,0 +1,19 @@
+﻿angular.module("sportsStore")
+	.controller("cartSummaryController", function ($scope, cart) {
+
+		$scope.cartData = cart.getProducts();
+		console.log("cart", cart);
+		console.log("$scope.cartData", $scope.cartData);
+
+		$scope.total = function () {
+			let total = 0;
+			for (let i = 0; i < $scope.cartData.length; i++) {
+				total += ($scope.cartData[i].price * $scope.cartData[i].count);
+			}
+			return total;
+		}
+
+		$scope.remove = function (id) {
+			cart.removeProduct(id);
+		}
+	});
