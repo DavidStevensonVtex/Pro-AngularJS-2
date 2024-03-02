db0c202e30484e36dc4e984c8845c36e12e37e42
David Stevenson <drstevenson1958@yahoo.com>
2024-03-02 17:49:16 -0500

    Add shiftFour() method to the second controller and view

--- ch13/controllers.js
+++ ch13/controllers.js
@@ -29,4 +29,12 @@ app.controller("secondChildCtrl", function($scope) {
     $scope.changeCase = function() {
         $scope.dataValue = $scope.dataValue.toLowerCase();
     }
+
+    $scope.shiftFour = function() {
+        var result = [] ;
+        angular.forEach($scope.dataValue.split(""), function ( char, index ) {
+            result.push(index < 4 ? char.toUpperCase() : char);
+        });
+        $scope.dataValue = result.join("");
+    }
 });
