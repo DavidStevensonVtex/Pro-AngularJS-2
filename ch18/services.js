d8e551fbf65a32fda6192d54b0b92832e4b4bd15
David Stevenson <drstevenson1958@yahoo.com>
2024-03-16 11:33:33 -0400

    Listing 18-10. Using the service method without prototypes in the services.js file

--- ch18/services.js
+++ ch18/services.js
@@ -1,18 +1,9 @@
-let baseLogger = function() {
-    this.messageCount = 0;
-    this.log = function ( msg ) {
-        console.log( this.msgType + ": " + (this.messageCount++) + " " + msg);
-    }
-};
-
-let debugLogger = function () { }
-debugLogger.prototype = new baseLogger() ;
-debugLogger.prototype.msgType = "Debug" ;
-
-let errorLogger = function() { };
-errorLogger.prototype = new baseLogger();
-errorLogger.prototype.msgType = "Error" ;
-
 angular.module("customServices", [])
-    .service("logService", debugLogger)
-    .service("errorService", errorLogger);
+    .service("logService", function() {
+        return { 
+            messageCount: 0,
+            log: function(msg) {
+                console.log("(LOG + " + (this.messageCount++) + ") " + msg);
+            }
+        };
+    });
