48015968dca349b9486aeb3a48decb8575955aab
David Stevenson <drstevenson1958@yahoo.com>
2024-03-23 14:40:36 -0400

    Creating the RESTful Service for Products

---
+++ ch21/ch21_ProAngularJS/Models/Product.cs
@@ -0,0 +1,27 @@
+﻿using Newtonsoft.Json;
+
+namespace SportsStore.Models
+{
+    public class Product
+    {
+        public Product(int id, string name, string category, decimal price)
+        {
+            this.Id = id;
+            this.Name = name;
+            this.Category = category;
+            this.Price = price;
+        }
+
+        [JsonProperty("id")]
+        public int Id { get; set; }
+
+        [JsonProperty("name")]
+        public string Name { get; set; }
+
+        [JsonProperty("category")]
+        public string Category { get; set; }
+
+        [JsonProperty("price")]
+        public decimal Price { get; set; }
+    }
+}
